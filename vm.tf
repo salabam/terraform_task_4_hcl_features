6dc3d65db3396805c6749427151c07894ef4e60d
salabam <pavelgrom.chaban@gmail.com>
2026-02-14 02:07:31 +0200

    Solution

--- vm.tf
+++ vm.tf
@@ -26,7 +26,7 @@ resource "azurerm_virtual_machine" "main" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-	lifecycle {
+  lifecycle {
     prevent_destroy = true
   }
   tags = {
